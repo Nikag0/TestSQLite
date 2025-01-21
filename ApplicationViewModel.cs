3837803ca08ccf23ce11542554bfc2cd26e61ab1
Nikag0 <Serito2@mail.ru>
2025-01-21 11:49:55 +0300

    добавление пользователя в таблицу

---
+++ ApplicationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSQLite
+{
+    public class ApplicationViewModel
+    {
+        ApplicationContext db = new ApplicationContext();
+        RelayCommand? addCommand;
+        RelayCommand? editCommand;
+        RelayCommand? deleteCommand;
+        public ObservableCollection<User> Users { get; set; }
+        public ApplicationViewModel()
+        {
+            db.Database.EnsureCreated();
+            db.Users.Load();
+            Users = db.Users.Local.ToObservableCollection();
+        }
+        // команда добавления
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand((o) =>
+                  {
+                      UserWindow userWindow = new UserWindow(new User());
+                      if (userWindow.ShowDialog() == true)
+                      {
+                          User user = userWindow.User;
+                          db.Users.Add(user);
+                          db.SaveChanges();
+                      }
+                  }));
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSQLite
+{
+    public class User : INotifyPropertyChanged
+    {
+        string? login;
+        string password;
+        public int Id { get; set; }
+        public string? Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
